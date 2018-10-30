750bec9619db74f28cb3b453f3b6b4f1cc359745
Yaroslsav <yyakimchuk@aplana.com>
2018-10-30 12:04:56 +0700

    Задание 6. Слова из файла в массив. Сколько раз слово встречается в массиве. Слово с максимальным количеством повторений.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,22 @@
 import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-
-    /*BufferedReader brr = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) {
-        System.out.println("Введите имя файла .present находящегося в каталоге С:/Present/");
-        myPresent.readFromFile(defaultPath+brr.readLine()+".present");
-        myPresent.print();
-    }*/
     public static void main(String[] args) throws IOException {
-        //String defaultPath = "C:/study/file.txt";
-        String[] splitResult;
-        try {
-            FileInputStream fstream = new FileInputStream("C:/study/file.txt");
-            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-            String strLine;
+        String defaultPath = "C:/study/file.txt";
+        System.out.print("Файл с входными данными должен называться 'file.txt'. \nРасположение файла: C:/study/ \nЕсли все верно, введите 'Да': ");
 
-            while ((strLine = br.readLine()) != null) {
-                splitResult = strLine.split(" ");
-                Arrays.sort(splitResult);
-                System.out.println(Arrays.toString(splitResult)+ "\n");
-                //System.out.println(strLine);
+        Scanner scaner = new Scanner(System.in);
+        String answer = scaner.nextLine();
+        scaner.close();
 
-                Set<String> wordsSet = new HashSet<String>(Arrays.asList(splitResult));
-                Map<String, Integer> exclusiveWords = new HashMap<>();
-                for (String word : wordsSet) {
-                    int count = 0;
-                    for (String w : splitResult) {
-                        if (word.equals(w)) count++;
-                    }
-                    exclusiveWords.put(word, count);
-                    System.out.println(word + " - " + count);
-                }
-
-               /* exclusiveWords.entrySet().stream()
-                        .sorted(Map.Entry.<String, Integer>comparingByValue().reversed());
-                        .forEach(System.out::println);*/
-                int maxValueInMap = (Collections.max(exclusiveWords.values()));
-                for (Map.Entry<String, Integer> entry : exclusiveWords.entrySet()) {
-                    if (entry.getValue() == maxValueInMap) {
-                        System.out.print("\nслово с максимальным количеством повторений: " + entry.getKey() + "\nколичество повторений: " +entry.getValue());
-                    }
-                }
-            }
-        } catch (
-                IOException e) {
-            System.out.println("Ошибка");
+        switch (answer) {
+            case "да":
+                ReadFileAndSort rfas = new ReadFileAndSort(defaultPath);
+                break;
+            default:
+                System.out.println("Подготовьте файл с входящими данными и повторите попытку.");
+                break;
         }
     }
 }
--- src/main/java/ReadFileAndSort.java
+++ src/main/java/ReadFileAndSort.java
@@ -1,18 +1,69 @@
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
+import java.util.*;
 
-/*public class ReadFileAndSort {
+public class ReadFileAndSort {
+    //String defaultPath = "C:/study/file.txt";
+    String[] splitResult;
+    Map<String, Integer> exclusiveWords = new HashMap<>();
 
-        public void readFromFile(String path) {
+    public ReadFileAndSort(String defaultPath) throws IOException {
+        readFromFile(defaultPath);
+        countRepeat(splitResult);
+        maxCount(exclusiveWords);
+    }
+
+    /**
+     * читаем файл в массив, сортируем
+     *
+     * @param defaultPath
+     * @throws IOException
+     */
+    public void readFromFile(String defaultPath) throws IOException {
         try {
-            FileInputStream fis = new FileInputStream(path);
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            this.gift=((Present)ois.readObject()).getList();
+            System.out.println("Слова из файла:");
+            String strLine;
+            FileInputStream fstream = new FileInputStream(defaultPath);
+            BufferedReader br = new BufferedReader(new InputStreamReader((fstream), "Cp1251"));
+            while ((strLine = br.readLine()) != null) {
+                splitResult = strLine.split(" ");
+                Arrays.sort(splitResult);
+                System.out.println(Arrays.toString(splitResult) + "\n");
+            }
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("Ошибка");
         }
     }
-}*/
+
+    /**
+     * считаем количество повторений каждого слова в массиве
+     *
+     * @param splitResult - сортированный массив
+     */
+    public void countRepeat(String[] splitResult) {
+        System.out.println("Количество повторений слов:");
+        Set<String> wordsSet = new HashSet<String>(Arrays.asList(splitResult));
+        for (String word : wordsSet) {
+            int count = 0;
+            for (String w : splitResult) {
+                if (word.equals(w)) count++;
+            }
+            exclusiveWords.put(word, count);
+            System.out.println(word + " - " + count);
+        }
+    }
+
+    /**
+     * выбираем слово с максимальным количеством повторений
+     *
+     * @param exclusiveWords
+     */
+    public void maxCount(Map<String, Integer> exclusiveWords) {
+        int maxValueInMap = (Collections.max(exclusiveWords.values()));
+        for (Map.Entry<String, Integer> entry : exclusiveWords.entrySet()) {
+            if (entry.getValue() == maxValueInMap) {
+                System.out.print("\nСлово с максимальным количеством повторений: " + entry.getKey() + "\nКоличество повторений: " + entry.getValue());
+            }
+        }
+    }
+}
+
